d03d84c5bbbc40d7f5275bffa90a3286e4c76e14
rizwanlodhi <rizwan.lodhi@gmail.com>
2026-01-21 19:22:03 -0600

    Only one state matched in the data set and written on the map.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US State Game")
@@ -7,5 +8,23 @@ screen.addshape(image)
 #shows the map
 turtle.shape(image)
 
-state_name = screen.textinput(title="Guess the state name",prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
 
+correct_guesses = 0
+
+answer_state = screen.textinput(title="Guess the state name",prompt="What's another state's name?")
+
+if answer_state in all_states:
+    correct_guesses += 1
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_date = data[data.state == answer_state]
+    t.goto(state_date.x.item(), state_date.y.item())
+    t.write(answer_state, """font=("Arial", 20, "bold")""")
+
+# print(data)
+
+# while correct_guesses < 50:
+screen.exitonclick()
